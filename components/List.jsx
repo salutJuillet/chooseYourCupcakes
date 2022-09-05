2909e968d4a7ae05bcaa78f571945630e8867976
salutJuillet <salut.juilllet@gmail.com>
2022-09-05 17:37:00 +0900

    Update List.jsx

--- components/List.jsx
+++ components/List.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
-import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native'
+import { SafeAreaView, ScrollView, SectionList, View, Text, Image, StyleSheet, TouchableOpacity, Button } from 'react-native'
 
 const List = ({navigation}) => {
+
+  const LIST = [
+    {
+      title:'Cupcakes',
+      lists:['Red Velvet', 'Devil', 'Classic Vanilla', 'Raspberry Cinnamon', 'Mint Chocolate', 'Blueberry White Chocolate'],
+      img:['RedVelvet.png', 'Devil.png', 'ClassicVanilla.png', 'RasberryCinnamon.png', 'MintChocolate.png', 'BlueberryWhiteChocolate.png']
+    },
+    {
+      title:'Cakes',
+      lists:['Black Forest'],
+      img:['BlackForest.png']
+    }
+  ]
+
+  const Item = ({title}) => {
+    // const {title, lists, img } = item;
+    <View style={styles.cupcakeContainer}>
+      {/* <Image source={require(`../assets/images/${img}`)} style={styles.cupcake} />
+      <Text>{lists}</Text> */}
+      <Text>{title}</Text>
+    </View>
+  }
+
   return (
     <SafeAreaView>
-        <Text>List Page</Text>
-        <Button title='back to Home' onPress={()=>navigation.navigate('Home')} />
+      <SectionList sections={LIST}
+                   keyExtractor={(item, index) => item + index}
+                   renderItem={({item}) => <Item title={item} />}
+                   renderSectionHeader={({section:{title}}) => (
+                    <Text style={styles.header}>{title}</Text>
+                   )} />
+        
+        {/* <Button title='back to Home' onPress={()=>navigation.navigate('Home')} /> */}
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  header:{},
+  cupcakeContainer:{},
+  cupcake:{},
+})
 
 export default List
